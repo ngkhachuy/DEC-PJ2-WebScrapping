5315e76fd07b9ce3bbd0e9a8a666075e0e7eb75c
ngkhachuy <nguyenhuy961127@gmail.com>
2023-03-21 23:49:39 +0700

    update 3.

--- 2.analysts.py
+++ 2.analysts.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     data.dropna(subset=['BRAND'], inplace=True)
 
     # Create visualization
-    fig, ax = plt.subplots(2, 2, figsize=(18, 7))
+    fig, ax = plt.subplots(3, 3, figsize=(15, 30))
 
     # 01. Brand name and number of products.
     brand_count = data.groupby(['BRAND']).size().reset_index(name='count')
@@ -33,20 +33,30 @@ if __name__ == '__main__':
     gs = ax[0, 1].get_gridspec()
     ax[0, 0].remove()
     ax[0, 1].remove()
-    axbig = fig.add_subplot(gs[0, :])
-    sns.barplot(y='BRAND', x='count', data=brand_count.tail(10), ax=axbig)
-    axbig.set_title('Top10 Featured Brands')
-    axbig.set(ylabel=None, xlabel='Number of Product')
+    axbig1 = fig.add_subplot(gs[0, :-1])
+    sns.barplot(y='BRAND', x='count', data=brand_count.tail(10), ax=axbig1)
+    axbig1.set_title('Top10 Featured Brands')
+    axbig1.set(ylabel=None, xlabel='Number of Product')
 
-    # 02. Distribution of price
+    # 02. Distribution of Price
     current_price = data[(data['CURRENT_PRICE'] > 0)]
-    current_price_under4000 = current_price[(current_price['CURRENT_PRICE'] <= 3000)]
-    current_price_over4000 = current_price[(current_price['CURRENT_PRICE'] > 3000)]
+    current_price_under4000 = current_price[(current_price['CURRENT_PRICE'] <= 2500)]
+    current_price_over4000 = current_price[(current_price['CURRENT_PRICE'] > 2500)]
     print(current_price['CURRENT_PRICE'].describe())
 
     sns.histplot(current_price_under4000, x='CURRENT_PRICE', ax=ax[1, 0])
-    ax[1, 0].set_title('Distribution of price (under $3000')
+    ax[1, 0].set_title('Distribution of price (under $2500)')
     sns.histplot(current_price_over4000, x='CURRENT_PRICE', ax=ax[1, 1])
-    ax[1, 1].set_title('Distribution of price (over $3000')
+    ax[1, 1].set_title('Distribution of price (over $2500)')
+
+    # 3. Distribution of Price by Brand
+    gs = ax[0, 1].get_gridspec()
+    ax[0, 2].remove()
+    ax[1, 2].remove()
+    ax[2, 2].remove()
+    axbig2 = fig.add_subplot(gs[0:, -1])
+    price_by_brand = data.loc[:, ['BRAND', 'CURRENT_PRICE']]
+    sns.scatterplot(price_by_brand, x='CURRENT_PRICE', y='BRAND', ax=axbig2)
+    axbig2.set(ylabel=None, xlabel='Distribution of price')
 
     plt.show()
